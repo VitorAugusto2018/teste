02859f73a4813695855e2a1fb7255d15ecfdc262
VitorAugusto2018 <36551827+VitorAugusto2018@users.noreply.github.com>
2022-10-18 22:00:08 -0300

    Delete htdocs directory

--- htdocs/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysql = new mysqli('localhost', 'root', '', 'teste');
-$mysql->set_charset('utf8');
-
-if ($mysql == FALSE) {
-    echo "Erro na conexão";
-}
--- htdocs/js/monitoramento.js
+++
@@ -1,8 +0,0 @@
-$.ajax({ 
-        url: '../src/ramais.php',        
-        type: 'post',
-        dataType: 'json',
-        success: function(output) {
-            alert(output);
-        }
-});
